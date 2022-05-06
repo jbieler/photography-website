e85581febffc4b6971b08cf83a901a5b135ed257
Jochen Bieler <jochen.bieler@web.de>
2022-05-06 09:21:46 +0200

    Fix broken asset path

--- next.config.js
+++ next.config.js
@@ -5,5 +5,5 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/photography-website/" : "",
+  assetPrefix: isProd ? "" : "", // will be empty on prod because site is served via own domain
 };
