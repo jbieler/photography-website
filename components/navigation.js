e30c9a3abd86a2c449ccdab311ab95f8648d6bcb
Jochen Bieler <jochen.bieler@web.de>
2022-05-09 22:15:01 +0200

    Optimize images

--- components/navigation.js
+++ components/navigation.js
@@ -11,9 +11,9 @@ function Navigation() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link  href="#home">Home</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
             <Nav.Link href="#angebot">Angebot</Nav.Link>
             <Nav.Link href="#ueber-mich">Über mich</Nav.Link>
-            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
             <Nav.Link  href="#kontakt">Kontakt</Nav.Link>
           </Nav>
         </Navbar.Collapse>
